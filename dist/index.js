3a174add663216647d8863351a5ee0607923c40c
Dobuzi <kimjw7981@gmail.com>
2020-07-24 12:41:40 +0900

    use class

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,11 @@
-const person = {
-    name: "Dobuzi",
-    age: 29,
-    gender: "male",
-};
+class Human {
+    constructor(name, age, gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
+const person = new Human("Dobuzi", 30, "male");
 const sayHello = (person) => {
     return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
